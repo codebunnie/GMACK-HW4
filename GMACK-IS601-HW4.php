84f07c9f27801ed4990d2d493772e8c2dd8b974b
gmm28 <31676798+codebunnie@users.noreply.github.com>
2017-09-26 17:05:58 -0400

    Added implode_array()

--- GMACK-IS601-HW4.php
+++ GMACK-IS601-HW4.php
@@ -33,251 +33,253 @@
   $obj->replace_array($schedule1, $schedule2);
   $obj->shuffle_array($schedule1);
   
+   /*
+  =================================================================================
+  =================================================================================
+  ====================================ARRAY========================================
+  =================================================================================
+  =================================================================================
+  */
+  $phoneArray = array('9174432245','8956587459', '5897456987', '97856984587');
+  $obj->implode_array($phoneArray);
+  
+  
+  
   class main {
 
     public function __construct() {
 		echo '<h1>IS601 - Homework # 4 </br> Author: Gabrielle Mack</h1> <hr>';     
     }
 	
-		public function print_array($array, $multiArray){
-		echo '<i><b>Function Name:</b>[1] print_array</br> <b>Array Function:</b> array_values</i> <hr></br>';
+	public function print_array($array, $multiArray){
+	echo '<i><b>Function Name:</b>[1] print_array</br> <b>Array Function:</b> array_values</i> <hr></br>';
+	
+	print_r(array_values($array));
+	echo '</br></br> Or Alternatively, a more readable format would be to loop through each element and add some formatting </br></br>';
+	
+	foreach($array as $key => $value){
+		echo "Index/Key: [$key]  Value: [$value]<br />\n";
+	}
+	
+	echo '</br></br> The same function can be used for 2 Dimensional arrays ...But still isnt that readable </br></br>';
+	print_r(array_values($multiArray));
+	
+	echo '</br></br>So we loop through each row and column and add some formatting </br></br>';
+	
+	for ($row = 0; $row < 6; $row++) {
 		
-		print_r(array_values($array));
-		echo '</br></br> Or Alternatively, a more readable format would be to loop through each element and add some formatting </br></br>';
+		echo '</br>Row/Index  '. $row. '&nbsp';
+		
+		for ($col = 0; $col < 3; $col++) {
+		echo " ".$multiArray[$row][$col]." ";
+		
+		}
+	}
+	echo '<hr>'; 
+	}
+		
+	public function sort_array($array, $multiArray){
+		echo '<i><b>Function Name:</b>[2] sort_array()</br> <b>Array Function:</b> sort()</i> <hr></br>';
+		echo '<b>Info Before Sort:</b> </br>';
+		foreach($array as $key => $value){
+			echo "Index/Key: [$key]  Value: [$value]<br />\n";
+		}
+		
+		echo '</br> <b>Info After Sort:</b> </br>';
+		
+		 sort($array, $sort_flags = SORT_STRING); 
 		
 		foreach($array as $key => $value){
 			echo "Index/Key: [$key]  Value: [$value]<br />\n";
 		}
 		
-		echo '</br></br> The same function can be used for 2 Dimensional arrays ...But still isnt that readable </br></br>';
-		print_r(array_values($multiArray));
-		
-		echo '</br></br>So we loop through each row and column and add some formatting </br></br>';
+		echo '</br><b>Info Before Sort:</b>';
+		for ($row = 0; $row < 6; $row++) {			
+			echo '</br>Row/Index  '. $row. '&nbsp';			
+				for ($col = 0; $col < 3; $col++) {
+					echo " ".$multiArray[$row][$col]." ";
+						}
+		}			
+		echo '</br></br> <b>Info After Sort:</b>';
+		sort($multiArray, $sort_flags = SORT_NUMERIC);
 		
 		for ($row = 0; $row < 6; $row++) {
-			
+		
 			echo '</br>Row/Index  '. $row. '&nbsp';
-			
+		
 			for ($col = 0; $col < 3; $col++) {
 			echo " ".$multiArray[$row][$col]." ";
-			
-			}
-		}
-		echo '<hr>'; 
-		}
-		
-		public function sort_array($array, $multiArray){
-			echo '<i><b>Function Name:</b>[2] sort_array()</br> <b>Array Function:</b> sort()</i> <hr></br>';
-			echo '<b>Info Before Sort:</b> </br>';
-			foreach($array as $key => $value){
-				echo "Index/Key: [$key]  Value: [$value]<br />\n";
-			}
-			
-			echo '</br> <b>Info After Sort:</b> </br>';
-			
-			 sort($array, $sort_flags = SORT_STRING); 
-			
-			foreach($array as $key => $value){
-				echo "Index/Key: [$key]  Value: [$value]<br />\n";
-			}
-			
-			echo '</br><b>Info Before Sort:</b>';
-			for ($row = 0; $row < 6; $row++) {			
-				echo '</br>Row/Index  '. $row. '&nbsp';			
-					for ($col = 0; $col < 3; $col++) {
-						echo " ".$multiArray[$row][$col]." ";
-			
 					}
-			}
-			
-			
-			
-			echo '</br></br> <b>Info After Sort:</b>';
-			sort($multiArray, $sort_flags = SORT_NUMERIC);
-			
-			for ($row = 0; $row < 6; $row++) {
-			
-				echo '</br>Row/Index  '. $row. '&nbsp';
-			
-				for ($col = 0; $col < 3; $col++) {
-				echo " ".$multiArray[$row][$col]." ";
-			
-				}
-			}
-		
-			echo'</br></br> Note: This function sorts by value and reorders the key/index pairs. <hr>';
-			
-			
 		}
+		echo'</br></br> Note: This function sorts by value and reorders the key/index pairs. <hr>';		
+	}
+
+	public function remove_duplicates($array, $multiArray){
+		echo '<i><b>Function Name:</b>[3] remove_duplicates()</br> <b>Array Function:</b> array_unique()</i> <hr></br>';
+		echo '<b>Info Before Sort:</b> </br>';
+			
+		sort($array, $sort_flags = SORT_NUMERIC); 
 		
-		public function remove_duplicates($array, $multiArray){
-			echo '<i><b>Function Name:</b>[3] remove_duplicates()</br> <b>Array Function:</b> array_unique()</i> <hr></br>';
-			echo '<b>Info Before Sort:</b> </br>';
-				
-			sort($array, $sort_flags = SORT_NUMERIC); 
-			
-			foreach($array as $key => $value){
-				echo "Index/Key: [$key]  Value: [$value]<br />\n";
-			}	
-			
-			$result = array_unique($array);
-				
-			echo '</br> <b>Info After Sort:</b> </br>';
-				
-			foreach($result as $key => $value){
-				echo "Index/Key: [$key]  Value: [$value]<br />\n";
-			}
-					
-			
-			echo '</br></br> <b>Info Before Sort:</b>';
-			
-			for ($row = 0; $row < 6; $row++) {
-			
-				echo '</br>Row/Index  '. $row. '&nbsp';
-			
-				for ($col = 0; $col < 3; $col++) {
-				echo " ".$multiArray[$row][$col]." ";
-			
-				}
-			}
-			$result = array_unique($multiArray, SORT_REGULAR);
-			
-			echo '</br></br> <b>Info After Sort:</b>';
-			
-			$x=count($result);
-						
-			for ($row = 0; $row < $x; $row++) {
-			
-				echo '</br>Row/Index  '. $row. '&nbsp';
-			
-				for ($col = 0; $col < 3; $col++) {
-					echo " ".$result[$row][$col]." ";
-			
-				}
-			}			
-		echo '<hr>';			
-		}
-		
-		public function count_array($array, $multiArray)
-		{
-			echo '<i><b>Function Name:</b>[4] count_array()</br> <b>Array Function:</b> array_count_values()</i> <hr></br>';
-					 
-			 foreach(array_count_values($array) as $key => $value){
-				echo "Index/Key: [$key]  Value: [$value]<br />\n";
-			}	
-				echo '<hr>';
-			/*
-			
-			$x=count($multiArray);
-						
-			for ($row = 0; $row < $x; $row++) {
-			
-				echo '</br>Row/Index  '. $row. '&nbsp';
-			
-				for ($col = 0; $col < 3; $col++) {
-					echo 'Count:'.(array_count_values($multiArray[$x]));
-			
-				}
-			}	*/
-			
-		}
-		public function diff_array($schedule1, $schedule2){
-			echo '<i><b>Function Name:</b>[5] diff_array()</br> <b>Array Function:</b> array_diff()</i> <hr></br>';
-			echo 'Schedule1: ';
-			foreach($schedule1 as $key => $value){
-				echo "[$key] [$value] &nbsp";
-			}
-			echo '</br>Schedule2: ';
-			foreach($schedule2 as $key => $value){
-				echo "[$key] [$value] &nbsp";
-			}
-			$result = array_diff($schedule1, $schedule2);
-			echo '<br><br>Array Differences:  ';
-			foreach($result as $key => $value){
-				echo "[$key] [$value] &nbsp";
-			}
-			echo '<hr>';
-		}	
-			public function merge_array($schedule1, $schedule2, $multiArray){
-			echo '<i><b>Function Name:</b>[6] merge_array()</br> <b>Array Function:</b> merge_array()</i> <hr></br>';
-			echo 'Schedule1: ';
-			foreach($schedule1 as $key => $value){
-				echo "[$key] [$value] &nbsp";
-			}
-			echo '</br>Schedule2: ';
-			foreach($schedule2 as $key => $value){
-				echo "[$key] [$value] &nbsp";
-			}
-			$result = array_merge($schedule1, $schedule2);
-			echo '<br><br>Merged Array:  </br>';
-			
-			foreach($result as $key => $value){
-				echo "Index/Key: [$key]  Value: [$value]<br />\n";
-			}			
-			echo ' <hr> ';		
-			}
-			
-			public function search_array($schedule1){
-			echo '<i><b>Function Name:</b>[7] search_array()</br> <b>Array Function:</b> array_search()</i> <hr></br>';
-			echo 'Schedule1: ';
-			foreach($schedule1 as $key => $value){
-				echo "[$key] [$value] &nbsp";
-			}
-			echo '<BR>Search String: "CS602"<br>';
-			echo 'First instance for "CS602" in $schedule1:  ['.array_search('CS602', $schedule1).']'; 
-			echo '<hr>';
-					} 
-									
-			public function multiply_array($array){
-			echo '<i><b>Function Name:</b>[8] multiply_array()</br> <b>Array Function:</b> product_array()</i> <hr></br>';
-			echo 'Array Values: <br><br>';
-			foreach($array as $key => $value){
+		foreach($array as $key => $value){
 			echo "Index/Key: [$key]  Value: [$value]<br />\n";
-			}
-			echo '<br>Array Product is: '.array_product($array).'<br>';
-			
-			echo '<hr>';
-			}
-			
-			public function replace_array($schedule1, $schedule2){
-			echo '<i><b>Function Name:</b>[9] replace_array()</br> <b>Array Function:</b> array_replace()</i> <hr></br>';
-			echo 'Schedule1: ';
-			foreach($schedule1 as $key => $value){
-				echo "[$key] [$value] &nbsp";
-			}
-			echo '</br>Schedule2: ';
-			foreach($schedule2 as $key => $value){
-				echo "[$key] [$value] &nbsp";
-			}
-			echo '<br><br>Replace the elements in $schedule1 with the elements in $schedule2 <br>';
-			
-			$result = array_replace($schedule1, $schedule2);
-			
-			echo 'Replaced Schedule1: ';
-			
-			foreach($result as $key => $value){
-				echo "[$key] [$value] &nbsp";
-			}
-			echo '<hr>';
-			
-			}
+		}	
 		
-			public function shuffle_array($schedule1){
-			echo '<i><b>Function Name:</b>[10] shuffle_array()</br> <b>Array Function:</b> shuffle()</i> <hr></br>';
+		$result = array_unique($array);
 			
-			 sort($schedule1, $sort_flags = SORT_STRING); 
-			echo 'Sorted Array:  <br>';
-			foreach($schedule1 as $key => $value){
-				echo "Index/Key: [$key]  Value: [$value]<br />\n";
+		echo '</br> <b>Info After Sort:</b> </br>';
+			
+		foreach($result as $key => $value){
+			echo "Index/Key: [$key]  Value: [$value]<br />\n";
+		}
+						
+		echo '</br></br> <b>Info Before Sort:</b>';
+		
+		for ($row = 0; $row < 6; $row++) {
+		
+			echo '</br>Row/Index  '. $row. '&nbsp';
+		
+			for ($col = 0; $col < 3; $col++) {
+			echo " ".$multiArray[$row][$col]." ";
+		
 			}
-			shuffle($schedule1);
-			 echo '<br> Shuffled Array: </br>'; 	
-			foreach($schedule1 as $key => $value){
-				echo "Index/Key: [$key]  Value: [$value]<br />\n";
+		}
+		$result = array_unique($multiArray, SORT_REGULAR);
+		
+		echo '</br></br> <b>Info After Sort:</b>';
+		
+		$x=count($result);
+					
+		for ($row = 0; $row < $x; $row++) {
+		
+			echo '</br>Row/Index  '. $row. '&nbsp';
+		
+			for ($col = 0; $col < 3; $col++) {
+				echo " ".$result[$row][$col]." ";
+		
 			}
+		}			
+	echo '<hr>';			
+	}
+
+	public function count_array($array, $multiArray) {
+		echo '<i><b>Function Name:</b>[4] count_array()</br> <b>Array Function:</b> array_count_values()</i> <hr></br>';
+				 
+		foreach(array_count_values($array) as $key => $value){
+			echo "Index/Key: [$key]  Value: [$value]<br />\n";
+		}	
 			echo '<hr>';
+		/*
+		
+		$x=count($multiArray);
+					
+		for ($row = 0; $row < $x; $row++) {
+		
+			echo '</br>Row/Index  '. $row. '&nbsp';
+		
+			for ($col = 0; $col < 3; $col++) {
+				echo 'Count:'.(array_count_values($multiArray[$x]));
+		
 			}
-			
+		}	*/
+		
+	}
+	public function diff_array($schedule1, $schedule2){
+		echo '<i><b>Function Name:</b>[5] diff_array()</br> <b>Array Function:</b> array_diff()</i> <hr></br>';
+		echo 'Schedule1: ';
+		foreach($schedule1 as $key => $value){
+			echo "[$key] [$value] &nbsp";
+		}
+		echo '</br>Schedule2: ';
+		foreach($schedule2 as $key => $value){
+			echo "[$key] [$value] &nbsp";
+		}
+		$result = array_diff($schedule1, $schedule2);
+		echo '<br><br>Array Differences:  ';
+		foreach($result as $key => $value){
+			echo "[$key] [$value] &nbsp";
+		}
+		echo '<hr>';
+	}	
+	
+	public function merge_array($schedule1, $schedule2, $multiArray){
+	echo '<i><b>Function Name:</b>[6] merge_array()</br> <b>Array Function:</b> merge_array()</i> <hr></br>';
+	echo 'Schedule1: ';
+	foreach($schedule1 as $key => $value){
+		echo "[$key] [$value] &nbsp";
+	}
+	echo '</br>Schedule2: ';
+	foreach($schedule2 as $key => $value){
+		echo "[$key] [$value] &nbsp";
+	}
+	$result = array_merge($schedule1, $schedule2);
+	echo '<br><br>Merged Array:  </br>';
+	
+	foreach($result as $key => $value){
+		echo "Index/Key: [$key]  Value: [$value]<br />\n";
+	}			
+	echo ' <hr> ';		
+	}
+	
+	public function search_array($schedule1){
+	echo '<i><b>Function Name:</b>[7] search_array()</br> <b>Array Function:</b> array_search()</i> <hr></br>';
+	echo 'Schedule1: ';
+	foreach($schedule1 as $key => $value){
+		echo "[$key] [$value] &nbsp";
+	}
+	echo '<BR>Search String: "CS602"<br>';
+	echo 'First instance for "CS602" in $schedule1:  ['.array_search('CS602', $schedule1).']'; 
+	echo '<hr>';
+	} 
+							
+	public function multiply_array($array){
+	echo '<i><b>Function Name:</b>[8] multiply_array()</br> <b>Array Function:</b> product_array()</i> <hr></br>';
+	echo 'Array Values: <br><br>';
+	foreach($array as $key => $value){
+	echo "Index/Key: [$key]  Value: [$value]<br />\n";
+	}
+	echo '<br>Array Product is: '.array_product($array).'<br>';
+	
+	echo '<hr>';
+	}
+	
+	public function replace_array($schedule1, $schedule2){
+	echo '<i><b>Function Name:</b>[9] replace_array()</br> <b>Array Function:</b> array_replace()</i> <hr></br>';
+	echo 'Schedule1: ';
+	foreach($schedule1 as $key => $value){
+		echo "[$key] [$value] &nbsp";
+	}
+	echo '</br>Schedule2: ';
+	foreach($schedule2 as $key => $value){
+		echo "[$key] [$value] &nbsp";
+	}
+	echo '<br><br>Replace the elements in $schedule1 with the elements in $schedule2 <br>';
+	
+	$result = array_replace($schedule1, $schedule2);
+	
+	echo 'Replaced Schedule1: ';
+	
+	foreach($result as $key => $value){
+		echo "[$key] [$value] &nbsp";
+	}
+	echo '<hr>';	
+	}
+
+	public function shuffle_array($schedule1){
+	echo '<i><b>Function Name:</b>[10] shuffle_array()</br> <b>Array Function:</b> shuffle()</i> <hr></br>';
+	
+	sort($schedule1, $sort_flags = SORT_STRING); 
+	echo 'Sorted Array:  <br>';
+	foreach($schedule1 as $key => $value){
+		echo "Index/Key: [$key]  Value: [$value]<br />\n";
+	}
+	shuffle($schedule1);
+	 echo '<br> Shuffled Array: </br>'; 	
+	foreach($schedule1 as $key => $value){
+		echo "Index/Key: [$key]  Value: [$value]<br />\n";
+	}
+	echo '<br><hr>';
+	}
+	
 	 /*
   =================================================================================
   =================================================================================
@@ -287,10 +289,27 @@
   */		
 		
 	
+/*methinks lets try and focus on injection prevention */
 
 
 
+	public function implode_array($string)
+	{
+	$phoneArray = array('9174432245','8956587459', '5897456987', '9785698587');
+	echo '<i><b>Function Name:</b>[1] implode_array()</br> <b>String Function:</b> implode()</i> <hr></br>';
 
+	echo 'Current Contents of Phone Array: <br>';
+	foreach($phoneArray as $key => $value){
+					echo "[$key] [$value] <br>";
+				}
+				
+	echo '<br>Imploded Contents: <br>';
+	$results = implode("<br>   ",$phoneArray);
+	print_r($results);
+
+	}
+
+	
 
 
 	
